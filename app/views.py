28f1e354b25f1b0569aac45ded0670d720d6a1c4
ViAchKoN <Vikon113@mail.ru>
2021-05-06 14:14:12 +0300

    Commit Feature update: 1. Added dataclasses for more convenient way to work with database;

--- app/views.py
+++ app/views.py
@@ -1,15 +1,14 @@
-from datetime import (
-    datetime,
-    timedelta
-)
-
 from aiohttp import web
 from aiohttp_apispec import (
     request_schema,
     docs
 )
 
-import db
+from datacls import (
+    UserDC,
+    JobOfferDC,
+    JobCandidateDC
+)
 
 from schemas import (
     Message,
@@ -28,53 +27,36 @@ async def index(request: web.Request) -> web.Response:
 
 
 async def get_job_offer_data(request):
+    u = UserDC()
+    jo = JobOfferDC()
+    jc = JobCandidateDC()
+
     job_offer_id = request['data']['job_offer_id']
+    phone_data = request['data']['phone']
 
     async with request.app['pool'].acquire() as connection:
-        records = await connection.fetch('''
-            SELECT
-                t1.candidate_id, t2.phone, t3.department, t3.manager
-                , t3.salary, t3.start_date, t4.name
-            FROM
-                (SELECT
-                    candidate_id , job_offer_id
-                FROM
-                    job_candidates jc
-                WHERE
-                    job_offer_id = $1) AS t1
-                LEFT JOIN
-                (SELECT
-                    id, phone
-                FROM
-                    users u
-                WHERE
-                    user_group_id = 2) AS t2 ON t1.candidate_id = t2.id
-                LEFT JOIN
-                (SELECT
-                    *
-                FROM
-                    job_offers jo
-                ) AS t3 ON t3.id = t1.job_offer_id
-                LEFT JOIN
-                (SELECT
-                    *
-                FROM
-                    users u
-                ) AS t4 ON t4.id = t3.employer_id
-        ''', job_offer_id)
+        job_offer = await jo.get_job_offer_by_id(connection, job_offer_id)
 
-        if len(records) == 0:
-            raise CustomException(message='No job offer has been found with id: {0}.'.format(job_offer_id), status=422)
+        if not job_offer:
+            raise CustomException(message='No job offer has been found with id: {0}.'.format(
+                job_offer_id), status=422)
 
-        phone = request['data']['phone']
+        candidates_list = await u.get_candidates_list_by_phone(connection, phone_data)
 
-        data = [dict(q) for q in records]
+        if not candidates_list:
+            raise CustomException(message='Users with these phone numbers have been not found phone: {0}.'
+                                  .format(phone_data), status=422)
+        job_candidates_list = await jc.get_job_candidates_list_by_job_offer_id(connection, job_offer_id)
 
-        data = [item for item in data if item['phone'] == phone]
+        wrong_candidates = set([candidate.phone for candidate in candidates_list if
+                                candidate.id not in [job_candidate.candidate_id for job_candidate in job_candidates_list]])
 
-        if not data:
-            raise CustomException(message='User with phone number: {0} is not a candidate for job offer with id: {1}.'.format(phone, job_offer_id), status=403)
-    return data
+        if wrong_candidates:
+            raise CustomException(message='User with phone number: {0} is not a candidate for job offer with id: {1}.'.format(
+                phone_data, job_offer_id), status=403)
+        employer = await u.get_employer_by_id(connection, job_offer.employer_id)
+
+    return job_offer, employer, candidates_list
 
 
 @docs(
@@ -88,69 +70,33 @@ async def get_job_offer_data(request):
 )
 @request_schema(JobOfferWithPhones)
 async def create_job_offer(request: web.Request) -> web.json_response:
+    u = UserDC()
+    jo = JobOfferDC()
+    jc = JobCandidateDC()
+
     post_data = request['data']
     phone_list_data = post_data['phoneList']
     employer_id_data = post_data['employer_id']
 
     async with request.app['pool'].acquire() as connection:
-        records = await connection.fetch('''
-            SELECT
-                *
-            FROM
-                users
-            WHERE
-                user_group_id = 1
-                AND
-                id = $1
-            ''', employer_id_data)
-        if len(records) == 0:
+        user = await u.get_employer_by_id(connection, employer_id_data)
+
+        if not user:
             return web.json_response({'message': 'No employer with id: {0} has been found.'.format(employer_id_data)},
                                      status=422)
+        missing_phones = await user.get_missing_phones(connection, phone_list_data)
 
-        records = await connection.fetch('''
-            SELECT
-                id, phone
-            FROM
-                users
-            WHERE
-                phone IS NOT NULL
-                AND user_group_id = 2
-        ''')
-        user_phones = [dict(q) for q in records]
-        user_phones_dict = {element['id']: element['phone']
-                            for element in user_phones if element['phone'] in phone_list_data for k, v in element.items()}
-
-        found_phones = set(user_phones_dict.values()
-                           ).intersection(set(phone_list_data))
-        different_phones = list(set(phone_list_data) - found_phones)
-
-        if different_phones:
+        if missing_phones:
             return web.json_response({'message': 'Users with these phone numbers have been not found.',
-                                      'phoneList': different_phones},
+                                      'phoneList': missing_phones},
                                      status=422)
         async with connection.transaction():
-            res = await connection.fetch('''
-                INSERT INTO
-                    job_offers(
-                        employer_id,
-                        department,
-                        manager,
-                        salary,
-                        create_date
-                    )
-                VALUES
-                    ($1, $2, $3, $4, $5)
-                RETURNING id
-                ''', employer_id_data,
-                post_data['department'],
-                post_data['manager'],
-                post_data['salary'],
-                datetime.now()
-            )
+            job_offer = await jo.create_job_offer(connection, employer_id_data, request['data']['department'],
+                                                  request['data']['manager'], request['data']['salary'])
 
-            data = [(res[0].get('id'), item) for item in user_phones_dict]
+            candidates_list = await u.get_candidates_list_by_phone(connection, phone_list_data)
 
-            await connection.copy_records_to_table('job_candidates', records=data, columns=('job_offer_id', 'candidate_id'))
+            await jc.fill_job_canidates(connection, job_offer.id, [candidate.id for candidate in candidates_list])
 
     return web.json_response({'message': 'Successful.'}, status=200)
 
@@ -167,12 +113,15 @@ async def create_job_offer(request: web.Request) -> web.json_response:
 )
 @request_schema(ShowOffer)
 async def get_job_offer_details(request: web.Request) -> web.json_response:
+
     try:
-        data = await get_job_offer_data(request)
+        job_offer, employer, candidates_list = await get_job_offer_data(request)
     except CustomException as e:
         return web.json_response({'message': e.message}, status=e.status)
 
-    return web.json_response(JobOffer(exclude=['employer_id']).dump(data[0]), status=200)
+    return web.json_response(
+        {**JobOfferDC.Schema(exclude=['employer_id', 'accepted_by', 'start_date', 'id']).dump(job_offer),
+         **UserDC.Schema(exclude=['id', 'phone', 'user_group_id']).dump(employer)})
 
 
 @docs(
@@ -188,36 +137,19 @@ async def get_job_offer_details(request: web.Request) -> web.json_response:
 )
 @request_schema(SignOffer)
 async def sign_job_offer(request: web.Request) -> web.json_response:
-    start_date = request['data']['start_date']
-
-    min_date = datetime.now().date() + timedelta(days=1)
-    max_date = datetime.now().date() + timedelta(days=30)
-
-    if not min_date <= start_date <= max_date:
-        return web.json_response({'message': 'Wrong start date provided. Start date should be between {0} and {1}, provided: {2}.'
-                                  .format(min_date, max_date, start_date)}, status=422)
-
     try:
-        data = await get_job_offer_data(request)
+        job_offer, employer, candidates_list = await get_job_offer_data(request)
     except CustomException as e:
         return web.json_response({'message': e.message}, status=e.status)
 
-    async with request.app['pool'].acquire() as connection:
-        candiate_id = data[0]['candidate_id']
-        job_offer_id = request['data']['job_offer_id']
+    jo = JobOfferDC()
+    candidate_id = candidates_list[0].id
 
-        res = await connection.fetch('''
-            UPDATE
-                job_offers
-            SET
-                accepted_by = COALESCE(accepted_by, $1)
-                , start_date = COALESCE(start_date, $2)
-            WHERE
-                id = $3
-            RETURNING accepted_by, start_date
-        ''',  candiate_id, start_date, job_offer_id)
-        if candiate_id == res[0].get('accepted_by'):
+    async with request.app['pool'].acquire() as connection:
+        job_offer = await jo.sign_job_offer(connection, candidate_id, request['data']['start_date'], request['data']['job_offer_id'])
+
+        if candidate_id == job_offer.accepted_by:
             return web.json_response({'message': 'Offer was signed successfully. Start date: {0}.'
-                                      .format(res[0].get('start_date'))}, status=200)
+                                      .format(job_offer.start_date)}, status=200)
         else:
             return web.json_response({'message': 'Offer is not longer active.'}, status=401)
